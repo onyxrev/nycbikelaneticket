4b2545c0016d277538f8b8ce68efbbbd1a3cb208
Dan Connor <danconn@danconnor.com>
2013-10-06 02:08:53 -0400

    nyc bike lane ticket is alive

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nycbikelaneticket::Application.config.secret_key_base = 'd04e30578925acda1b0389cc349735bf33727b634012486f707f09bd4920bc66e11050d0a0fdad474cc244c3dc2eafd6bec71d39f8b653429396b04cb89a06a2'
