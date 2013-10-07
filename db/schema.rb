4ca17af189295b0bf8412ad30dd535292fdf1c37
Dan Connor <danconn@danconnor.com>
2013-10-06 23:08:00 -0400

    better validation display

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,18 @@ ActiveRecord::Schema.define(version: 20131006183457) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "country_code"
+    t.string   "auth_hash"
   end
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "tickets", force: true do |t|
     t.string   "number"
