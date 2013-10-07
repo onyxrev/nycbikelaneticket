07f10aab7641b7bb492a50f6438d8b3e77a8ff56
Dan Connor <danconn@danconnor.com>
2013-10-07 00:04:00 -0400

    add some model unit tests

--- spec/models/plaintiff_spec.rb
+++ spec/models/plaintiff_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe Plaintiff do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "auth methods" do
+    before(:each) do
+      @email       = "scofflaw.biker@bike_messengers.net"
+      @postal_code = "10001"
+      @auth_string = "zomg"
+      @md5_hash    = "md5 hash yay"
+    end
+
+    describe "#self.generate_auth_hash" do
+      it "generates the auth string" do
+        Plaintiff.should_receive(:auth_string).with(@email, @postal_code)
+        Digest::MD5.stub(:hexdigest)
+
+        Plaintiff.generate_auth_hash(@email, @postal_code)
+      end
+
+      it "creates an MD5 hash the email and postal code" do
+        Plaintiff.stub(:auth_string).and_return(@auth_string)
+        Digest::MD5.should_receive(:hexdigest).with(@auth_string).and_return(@md5_hash)
+
+        Plaintiff.generate_auth_hash(@email, @postal_code).should == @md5_hash
+      end
+    end
+
+    describe "#self.for_email_and_postal_code" do
+      it "generates an auth hash for the lookup" do
+        Plaintiff.should_receive(:generate_auth_hash).with(@email, @postal_code)
+        Plaintiff.stub(:where).and_return(double(:first => nil))
+
+        Plaintiff.for_email_and_postal_code(@email, @postal_code)
+      end
+
+      it "finds the first Plaintiff with the auth hash" do
+        plaintiff = double
+
+        Plaintiff.stub(:generate_auth_hash).and_return(@md5_hash)
+        Plaintiff.should_receive(:where).with(:auth_hash => @md5_hash).and_return(double(:first => plaintiff))
+
+        Plaintiff.for_email_and_postal_code(@email, @postal_code).should == plaintiff
+      end
+    end
+  end
+
+  describe "#to_hash" do
+    it "returns a hash of useful keys" do
+      fullname    = "Michael Capello"
+      created_at = Time.now
+
+      plaintiff = Plaintiff.new(:fullname => fullname, :created_at => created_at)
+
+      plaintiff.to_hash.should == { :fullname => fullname, :created_at => created_at }
+    end
+  end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Ticket do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#to_hash" do
+    it "returns a hash of useful data" do
+      plaintiff_fullname =  "Michael Capello"
+
+      ticket_data = {
+        :created_at => Time.now,
+        :date       => Date.today,
+        :latitude   => 40.551042,
+        :longitude  => -74.05663
+      }
+
+      plaintiff = Plaintiff.create(:fullname => plaintiff_fullname, :postal_code => "10001", :email => "zip@zip.com", :country => "United States")
+      ticket = plaintiff.tickets.new(ticket_data)
+
+      ticket.to_hash.should == ticket_data.merge(:fullname => plaintiff_fullname)
+    end
+  end
 end
